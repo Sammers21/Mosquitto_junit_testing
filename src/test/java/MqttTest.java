9f005088d649bdbe7e34ee79274ee232003da148
Pavel Drankov <titantins@gmail.com>
2017-06-14 23:07:38 +0300

    rename test class

--- src/test/java/MqttTest.java
+++ src/test/java/MqttTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
-public class mqttTest {
+public class MqttTest {
     @Before
     public void before() throws IOException, InterruptedException {
         executeCommand("/bin/bash deploy.sh");
